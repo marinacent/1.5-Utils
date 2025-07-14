3eeef7df4a5c03ed74dbea3b19d9ead0e1199eb2
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-14 18:26:52 +0200

    fix: use file separator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         String dirPath = "src";
-        String outPath = "src/data/output.txt";
+        String outPath = "src" + File.separator + "data" + File.separator + "output.txt";
 
         ContentLister.listDirContent(dirPath);
 

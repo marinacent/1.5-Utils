b12c6265e9d7354a1eb3413f2cf832ed5fd1917c
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-14 18:33:55 +0200

    feat: create serializable person class

---
+++ src/main/java/Person.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String name;
+    private String age;
+
+    public Person(String age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}

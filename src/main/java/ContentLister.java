4ed7a1b0a0c552ff2097061d41af2e1ae6a5cf4c
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-14 18:09:14 +0200

    fix: make listdircontents return an array of files

--- src/main/java/ContentLister.java
+++ src/main/java/ContentLister.java
@@ -7,46 +7,45 @@ import java.util.Arrays;
 public class ContentLister {
     private String dirPath;
 
-    public static void listDirContent(String dirPath) {
+    public static File[] listDirContent(String dirPath) {
         // TO DO: THROW APPROPRIATE EXCEPTIONS! ** check if statements
         File dir = new File(dirPath);
+        File[] files = null;
         if (dir.exists() && dir.isDirectory()) {
-            String[] files = dir.list();
+            files = dir.listFiles();
             if (files != null) {
-                Arrays.sort(files);
-                for (String file: files) {
+                Arrays.sort(files, (f1, f2) -> f1.getName().compareToIgnoreCase(f2.getName()));
+                for (File file : files) {
                     System.out.println(file);
-            }
+                }
             }
         }
+        return files;
     }
 
     public static void listRecursively(File dir, BufferedWriter writer) throws IOException {
         // handle exceptions
         // print --> D/F + last modified + indents¿?¿??¿?¿¿??¿?¿
-        File[] elements = dir.listFiles();
-        if (elements != null) {
-            Arrays.sort(elements, (f1, f2) -> f1.getName().compareToIgnoreCase(f2.getName()));
-            for (File element: elements) {
-                // System.out.println(element);
-                writer.write(element.getName());
-                writer.newLine();
-                if (element.isDirectory()) {
-                    listRecursively(element, writer);
-                }
+        File[] elements = listDirContent(dir.getPath());
+        for (File element : elements) {
+            // System.out.println(element);
+            writer.write(element.getName());
+            writer.newLine();
+            if (element.isDirectory()) {
+                listRecursively(element, writer);
             }
-
         }
+
     }
 
     public static void listDirTree(String dirPath, String outPath) throws IOException {
-        // TO DO: handle exception!
-        File dir = new File(dirPath);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outPath))) {
-            if (dir.exists() && dir.isDirectory()) {
-                listRecursively(dir, writer);
-            }
+    // TO DO: handle exception!
+    File dir = new File(dirPath);
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(outPath))) {
+        if (dir.exists() && dir.isDirectory()) {
+            listRecursively(dir, writer);
         }
     }
+}
 
 }

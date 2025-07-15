2b140f45ce743c271a5a8bc83ab1487c91a43ed0
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-15 11:03:42 +0200

    fix: throw exception when input is null

--- src/main/java/ConfigLoader.java
+++ src/main/java/ConfigLoader.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -5,23 +6,21 @@ import java.util.Properties;
 public class ConfigLoader {
     private Properties properties;
 
-    public ConfigLoader(String fileName) {
+    public ConfigLoader(String fileName) throws IOException {
         properties = new Properties();
 
         try (InputStream input = getClass().getClassLoader().getResourceAsStream(fileName)) {
             if (input == null) {
-                System.out.println("Couldn't access " + fileName);
-                return;
+                throw new FileNotFoundException("Couldn't access " + fileName);
             }
+
             properties.load(input);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
     }
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+        }
+        
     }
-
-
-}
